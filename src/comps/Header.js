24951451e9a6bd8529eb2be7229e8c928221533e
Puscas Cristian <pusckyh@gmail.com>
2020-08-19 15:16:53 +0300

    added login google button that logs in users

--- src/comps/Header.js
+++ src/comps/Header.js
@@ -1,12 +1,21 @@
 /* eslint-disable jsx-a11y/accessible-emoji */
-import React from 'react'
-
+import React, { useState } from 'react'
+import Button from '@material-ui/core/Button';
+import SvgIcon from '@material-ui/core/Icon';
+import { googleSignIn } from '../firebase/config';
 const Header = () => {
+    const [Logged, setLogged] = useState(false)
+    const handleGoogleAuth = () => {
+        googleSignIn();
+    }
     return (
         <div className="header-wrapper">
             <img src="https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png" alt="React Logo" />
             <h1>Publish a thought! 💭</h1>
-            <img src="https://firebase.google.com/downloads/brand-guidelines/PNG/logo-logomark.png" alt="Firebase Logo" />
+            <div className="firebase-wrapper">
+                <img src="https://firebase.google.com/downloads/brand-guidelines/PNG/logo-logomark.png" alt="Firebase Logo" />
+                <Button onClick={handleGoogleAuth}><SvgIcon style={{ padding: "0 10px"}}><img style={{ width: "20px"}} src="https://www.google.com/images/branding/googleg/1x/googleg_standard_color_128dp.png" /></SvgIcon> Login with Google</Button>
+            </div>
         </div>
     )
 }
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -13,7 +13,28 @@ const firebaseApp = firebase.initializeApp({
 });
 
 firebase.analytics();
+const googleSignIn = () => {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        return result;
+        // ...
+      }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+}
+
 
 const db = firebaseApp.firestore();
 const timestamp = firebase.firestore.FieldValue.serverTimestamp();
-export { db, timestamp }
+export { db, timestamp, googleSignIn }
